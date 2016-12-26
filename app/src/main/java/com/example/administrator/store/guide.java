481172ee241243ac6d37b42044a7acc3ff192850
qq1870163860 <1870163860@qq.com>
2016-12-27 01:03:42 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/store/guide.java
@@ -0,0 +1,22 @@
+package com.example.administrator.store;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+
+import java.util.List;
+
+
+public class guide extends Activity {
+
+private ViewPager vp;
+private Viewpageradpter vpa;
+    private List<view> views;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
